43cf19ffde874ac5edf573b01be8a408d6a885e5
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-11-13 10:35:41 -0300

    mais comments

--- shoot.py
+++ shoot.py
@@ -4,7 +4,9 @@ dicionario_direction = {
 
 # state 0 = guardada, 1 = em movimento, 2 = no chao
 
+# classe que representa o tiro
 class shoot:
+    # definimos as variaveis direcao, posicao, nmr da imagem, velocidade e força
     def __init__(self, direction, position, image_number):
         self.__state = 0
 
@@ -31,43 +33,58 @@ class shoot:
         
         self.__current_image = self.images[direction]
 
+    # função para mover o tiro de acordo com o incremento
     def move(self):
+        # incrementa-se a posiçao x e y
         self.__position[0] += self.incremento_x
         self.__position[1] += self.incremento_y
         
+        # diminui-se 1 na força
         self.__power -= 1
         
+        # se a força é 0, paramos a flecha
         if(self.__power <= 0):
-            self.__current_image = self.images[8]
-            self.__state = 2
+            self.__current_image = self.images[8] # coloca-se a imagem da flecha caída
+            self.__state = 2                      # mudamos o estado para a flecha no chao
 
+    # getter para o estado
     @property
     def state(self):
         return self.__state
+
+    # setter para o estado
     @state.setter
     def state(self, state):
         if(state == 2):
             self.__current_image = self.images[8]
         self.__state = state
     
+    # getter para a direcao
     @property
     def direction(self):
         return self.__direction
+
+    # setter para a direcao
     @direction.setter
     def direction(self, state):
         self.__direction = state
 
+    # getter para a posicao
     @property
     def position(self):
         return self.__position
+
+    # setter para a posição
     @position.setter
     def position(self, state):
         self.__position = state
 
+    # getter para a imagem atual
     @property
     def current_image(self):
         return self.__current_image
 
+    # setter para a imagem atual
     @current_image.setter
     def current_image(self, direction):
         self.__current_image = self.images[direction]
--- sprite.py
+++ sprite.py
@@ -1,23 +1,31 @@
 import pygame
 import time
+
+# classe responsável pelo controle das animações dos personagens
 class sprite:
     def __init__(self, player_number, direction):
+        # se o player for 1, colocamos a imagem da shotgun
         if(player_number == 1):
             self.path = "images//sprites//shotgun//"
             self.__rotate = False            
         else:
+            # se for o jogador dois, passamos a imagem do rifle
             self.path = "images//sprites//rifle//"
             self.__rotate = True
-
+        # setamos as outras variáveis adicionais
         self.direction = direction
         self.count = 0
-        self.images = []        
+        self.images = []
+
+        # adicionamos as imagens no vetor        
         for i in range(16):
             nmr = str(i+1).zfill(2)
             img = self.path + f"Armature_run_{nmr}.png"
             self.images.append(img)
 
+    # método responsável pela animação do andar do pinguim
     def move(self, direction, position, tela):
+        # mudamos a variavel         
         if(direction == 4 or direction == 0):
             pass
         elif(direction <= 3):
@@ -25,16 +33,19 @@ class sprite:
         else:
             self.rotate = True
 
+        # incrementamos 1 no count, para mudar o numero da animacao
         self.count = self.count + 1
         if(self.count >= 16):
             self.count = 0
 
         self.draw_walk(position, tela)
 
+    # função que para a sprite  na posicao inicial
     def stop(self, position, tela):
         self.count = 0
         self.draw_walk(position, tela)
 
+    # função que realiza a animação de tiro, de acordo com a direção
     def shoot(self, direction, position, tela, is_action):
         if(is_action):
             self.count = 0
@@ -49,18 +60,22 @@ class sprite:
         else:
             self.draw_shoot("Armature_jump_shooting_down_", position, tela)
 
+    # propriedade que pega a imagem atual, no vetor de imagens
     @property
     def current_image(self):
         return self.images[self.count]
 
+    # propriedade que indica o lado da imagem
     @property
     def rotate(self):
         return self.__rotate
 
+    # setter para a direção da imagem
     @rotate.setter
     def rotate(self, state):
         self.__rotate = state
 
+    # função que desenha o  andar da animação
     def draw_walk(self, position, tela):
         player_img = pygame.image.load(self.current_image)
         player_img = pygame.transform.flip(player_img, self.rotate, False)
@@ -68,6 +83,7 @@ class sprite:
         player_img.set_colorkey((255, 255, 255))
         tela.blit(player_img, (position[0], position[1]))
 
+    # função que desenha o tiro da animação
     def draw_shoot(self, img, position, tela):        
         player_img = pygame.image.load(self.path + img + str(self.count) + ".png")
         player_img = pygame.transform.flip(player_img, self.rotate, False)
